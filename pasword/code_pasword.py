eb8db1d59e37cfd213ffa19306bb563738b671a1
nandaUnical <rtgmfr98t59z504s@studenti.unical.it>
2023-02-21 20:29:17 +0100

    Prueba uno

--- pasword/code_pasword.py
+++ pasword/code_pasword.py
@@ -1,3 +1,64 @@
+import json as js
 
+class pasword :
+    def __init__(self,texto):
+        self.pwd = texto #Lista de paswords
+        self.response = [{'pwd':'','issues': []}]*len(texto)
+        for i in range (len(texto)) :
+            self.response[i]['pwd'] = texto[i]
+
+    #Crear metodos para verificar cada uno de los requisitos
+    def validate_pwd_size (self):
+        for i in range (len(self.pwd)) :
+            #cada i es una pasword
+            if len(self.pwd[i]) < 8 or len(self.pwd[i]) > 16 :
+                self.response[i]['issues'].append('Pasword should have at least 8 characters and no more than 16.')
+                break
+
+
+    def validate_upper_case (self):
+        for i in range (len(self.pwd)) :
+            if sum(1 for c in self.pwd[i] if c.isupper()) < 1:
+                self.response[i]['issues'].append('Pasword should have at least one upper case')
+                break
+
+
+    def have_4diff_char (self):
+        for i in range (len(self.pwd)) :
+            if set(self.pwd[i]) < 4 :
+                self.response[i]['issues'].append('Pasword should have at least 4 differents characters')
+                break
+        
+
+    def have_adyacent_char (self):
+        for i in range (len(self.pwd)) :
+            for j in len(1,self.pwd[i]) :
+                if self.pwd[i][j] == self.pwd[i][j-1] :
+                    self.response[i]['issues'].append('Pasword cannot have consecutive equal characters')
+                    break        
+
+
+def crear_datos ():  
+    dicc = { "users" : [{"name": "nandaUnical","pwd": "12345"}, {"name": "brunoUnical","pwd" : "qwert"}, {"name":"amandaUnical","pwd":"1q2w3e"}] }
+    with open("data.json", "w") as f:
+        js.dump(dicc, f)
+
+
+def obtener_datos ():
+    with open('data.json', 'r') as f:
+        user_dicc = js.load(f)
+    #Obteniendo cada pasword
+    pwd = []#Paswords de todos los usuarios
+    for i in user_dicc:
+        #print (user_dicc[i])#lista de diccionarios
+        for j in user_dicc[i]:
+            #print (j)#cada diccionario en la lista
+            pwd.append(j['pwd'])
+    return pwd
+        
  
+crear_datos()
+texto = obtener_datos()
+
+a = pasword(texto)
 
--- tests/test_pasword.py
+++ tests/test_pasword.py
@@ -1,3 +1,36 @@
+import pytest
+import sys
+sys.path.insert(1,'C:/Users/hp/Documents/!Master/Agile/!!Second Apello/secondo_appello/pasword')
+import code_pasword as cp
 
- 
 
+@pytest.fixture
+def pasword_object():
+    texto = cp.obtener_datos()
+    object = cp.pasword(texto)
+    return object
+
+#@pytest.mark.parametrize('pwd',[('1234','qwert','1q2w3e')])
+
+def test_validate_pwd_size(pasword_object):
+    for elem in pasword_object.response :
+        print(elem)
+        #assert elem['issue'] == 'Pasword should have at least 8 characters and no more than 16.'
+
+'''
+def test_validate_pwd_size (pasword_object):
+    pasword_object.validate_pwd_size() 
+    
+    print (pasword_object.response)
+
+def test_have_upper_case ():
+    assert cp.have_upper_case('12W12') == True, 'It should have at least une uppercase latter'
+
+def test_differents_char ():
+    assert cp.have_4diff_char ('asdasdasd') == False , 'Have 4 differents characters'
+
+def test_have_adyacent_char() :
+    assert cp.have_adyacent_char ('12234') == True, 'Havent adyacent characters'
+    '''
+
+#test_validate_pwd_size(pasword_object)
